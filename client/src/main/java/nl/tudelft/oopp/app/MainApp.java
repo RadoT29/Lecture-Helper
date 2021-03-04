0ac08e454b5c6e82f7af2e27c9420526ca685935
Pedro Brandão <p.h.brandaodearaujo@student.tudelft.nl>
2021-03-04 17:03:35 +0100

    chore: Add new package for our app development with folder structure following the template

---
+++ client/src/main/java/nl/tudelft/oopp/app/MainApp.java
@@ -0,0 +1,9 @@
+package nl.tudelft.oopp.app;
+
+import nl.tudelft.oopp.app.views.SplashDisplay;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SplashDisplay.main(new String[0]);
+    }
+}
