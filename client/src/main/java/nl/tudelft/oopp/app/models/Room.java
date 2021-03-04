257571054b60d4542bd34be2dd64a4b0342c32e6
Pedro Brandão <p.h.brandaodearaujo@student.tudelft.nl>
2021-03-04 18:48:35 +0100

    feature(room): room creation feature
    add gson library
    add room entity model
    add POST route to create room
    add splash screen prototype

---
+++ client/src/main/java/nl/tudelft/oopp/app/models/Room.java
@@ -0,0 +1,12 @@
+package nl.tudelft.oopp.app.models;
+
+import java.util.UUID;
+
+public class Room {
+    public String name;
+    public UUID linkIdStudent;
+    public UUID linkIdModerator;
+    public boolean isOpen;
+
+
+}
---
+++ server/src/main/java/nl/tudelft/oopp/app/controllers/RoomController.java
@@ -0,0 +1,24 @@
+package nl.tudelft.oopp.app.controllers;
+
+import nl.tudelft.oopp.app.models.Room;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class RoomController {
+
+    /**
+     * GET Endpoint to retrieve a random quote.
+     *
+     * @return randomly selected {@link Room}.
+     */
+    @PostMapping("room")
+    @ResponseBody
+    public Room getNewRoomLinks(@RequestParam String name) {
+        return new Room(name);
+    }
+}
