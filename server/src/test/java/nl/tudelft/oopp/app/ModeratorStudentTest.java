ef028c357fa27f8bac238e7e266ab2fd9257e6ad
Rado Todorov <R.A.Todorov@student.tudelft.nl>
2021-03-06 12:57:29 +0100

    Fixed testing problem (tests passed localy, but failed in pipeline)

--- server/src/test/java/nl/tudelft/oopp/app/ModeratorStudentTest.java
+++ server/src/test/java/nl/tudelft/oopp/app/ModeratorStudentTest.java
@@ -8,15 +8,11 @@ import nl.tudelft.oopp.app.models.Moderator;
 import nl.tudelft.oopp.app.repositories.RoomRepository;
 import nl.tudelft.oopp.app.repositories.StudentRepository;
 import nl.tudelft.oopp.app.repositories.ModeratorRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ModeratorStudentTest {
     @Autowired
     private RoomRepository roomRepository;
@@ -29,11 +25,13 @@ public class ModeratorStudentTest {
 
 
     @Test
-    @Order(1)
     public void saveAndRetrieveStudent() {
+
         Room room = new Room("room name");
         roomRepository.save(room);
 
+        studentRepository.deleteAll();
+        moderatorRepository.deleteAll();
         Student student = new Student("Radoslav",room);
         studentRepository.save(student);
 
@@ -42,43 +40,45 @@ public class ModeratorStudentTest {
     }
 
     @Test
-    @Order(2)
     public void saveAndRetrieveModerator() {
         Room room = new Room("room name");
         roomRepository.save(room);
 
+        studentRepository.deleteAll();
+        moderatorRepository.deleteAll();
         Moderator mod = new Moderator("Rado",room);
         moderatorRepository.save(mod);
 
-        Moderator mod2 = moderatorRepository.getOne((long) 2);
+        Moderator mod2 = moderatorRepository.getOne((long) 1);
         assertEquals(mod, mod2);
-        assertEquals(mod.getRoomId().getId(), 2);
     }
 
     @Test
-    @Order(3)
     public void saveAndRetrieveRoomViaModerator() {
         Room room = new Room("OurRoom");
         roomRepository.save(room);
 
+        studentRepository.deleteAll();
+        moderatorRepository.deleteAll();
         Moderator mod = new Moderator("Pedro",room);
         moderatorRepository.save(mod);
 
-        Moderator moderator = moderatorRepository.getOne((long) 3);
+        Moderator moderator = moderatorRepository.getOne((long) 1);
         assertEquals(moderator.getRoomId(), room);
         assertEquals(moderator.getRoomId().getName(), "OurRoom");
     }
 
     @Test
-    @Order(4)
     public void saveAndRetrieveRoomViaStudent() {
         Room room = new Room("room name");
         roomRepository.save(room);
 
+        studentRepository.deleteAll();
+        moderatorRepository.deleteAll();
         Student stud = new Student("Pedro",room);
         studentRepository.save(stud);
 
-        Student student = studentRepository.getOne((long) 4);
+        Student student = studentRepository.getOne((long) 1);
         assertEquals(student.getRoomId(), room);
         assertEquals(student.getRoomId().getName(), "room name");
     }
