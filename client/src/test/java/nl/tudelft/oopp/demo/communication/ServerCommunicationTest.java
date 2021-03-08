e55601003e33e5c123b4343b8193dfb25d742923
Pedro Brandão <p.h.brandaodearaujo@student.tudelft.nl>
2021-03-08 17:37:57 +0100

    test(room): Add tests for rooms on server side and client side

--- client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
@@ -1,38 +1,14 @@
-package nl.tudelft.oopp.app.communication;
+package nl.tudelft.oopp.demo.communication;
 
-import com.google.gson.Gson;
-import nl.tudelft.oopp.app.models.Room;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.junit.jupiter.api.Test;
 
-public class SplashCommunication {
 
-    private static Gson gson = new Gson();
+public class ServerCommunicationTest {
 
-    private static HttpClient client = HttpClient.newBuilder().build();
-
-    /**
-     * Retrieves a quote from the server.
-     * @return the body of a get request to the server.
-     * @throws Exception if communication with the server fails.
-     */
-    public static Room postRoom(String name) {
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.noBody())
-                .uri(URI.create("http://localhost:8080/room?name=" + name.replace(" ","%20"))).build();
-        HttpResponse<String> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        if (response.statusCode() != 200) {
-            System.out.println("Status: " + response.statusCode());
-        }
-        return gson.fromJson(response.body(), Room.class);
+    @Test
+    public void testRandomQuote() {
+        assertNotNull(ServerCommunication.getQuote());
     }
-
 }
