6cef932a0e7e05f2424114bae6709751a944b400
Mauro Storti <m.storti@student.tudelft.nl>
2021-03-12 03:21:43 +0100

    test(Question): Adds tests for the Question Object

---
+++ server/src/test/java/nl/tudelft/oopp/app/QuestionTest.java
@@ -0,0 +1,106 @@
+package nl.tudelft.oopp.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import nl.tudelft.oopp.app.models.Question;
+import nl.tudelft.oopp.app.models.Room;
+import nl.tudelft.oopp.app.models.Student;
+import nl.tudelft.oopp.app.models.Moderator;
+import nl.tudelft.oopp.app.repositories.QuestionRepository;
+import nl.tudelft.oopp.app.repositories.RoomRepository;
+import nl.tudelft.oopp.app.repositories.StudentRepository;
+import nl.tudelft.oopp.app.repositories.ModeratorRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class QuestionTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private ModeratorRepository moderatorRepository;
+
+    /***
+     * This test saves a Question on the database and then retrieves it
+     * to assert that it's the same
+     */
+
+    @Test
+    public void saveAndRetrieveQuestion() {
+
+        //A question has a room associated to it
+        Room room = new Room("room name");
+        roomRepository.save(room);
+
+        //A question has a user associated to it
+        Student student = new Student("Radoslav",room);
+        studentRepository.save(student);
+
+        Question question = new Question("Does this test work?");
+        question.setRoom(room);
+        question.setUser(student);
+        questionRepository.save(question);
+
+
+        Question qestion2 = questionRepository.getOne(question.getId());
+        assertEquals(question, qestion2);
+    }
+
+    /***
+     * This test saves a Question on the database and then asserts
+     * that the associated Room is the same one
+     */
+
+    @Test
+    public void saveAndRetrieveRoomViaQuestion() {
+        Room room = new Room("OurRoom");
+        roomRepository.save(room);
+
+        Moderator mod = new Moderator("Pedro",room);
+        moderatorRepository.save(mod);
+
+        Question question = new Question("Does this test work?");
+        question.setRoom(room);
+        question.setUser(mod);
+        questionRepository.save(question);
+
+        Question question2 = questionRepository.getOne(question.getId());
+        assertEquals(question2.getRoom(), room);
+        assertEquals(question2.getRoom().getName(), "OurRoom");
+
+    }
+
+    /***
+     * This test saves a Question on the database and then asserts
+     * that the associated User is the same one
+     */
+
+    @Test
+    public void saveAndRetrieveUserViaQuestion() {
+        Room room = new Room("OurRoom");
+        roomRepository.save(room);
+
+        Moderator mod = new Moderator("Natalia",room);
+        moderatorRepository.save(mod);
+
+        Question question = new Question("Does this test work?");
+        question.setRoom(room);
+        question.setUser(mod);
+        questionRepository.save(question);
+
+        Question question2 = questionRepository.getOne(question.getId());
+        assertEquals(question2.getUser(), mod);
+        assertEquals(question2.getUser().getName(), "Natalia");
+
+    }
+
+}
