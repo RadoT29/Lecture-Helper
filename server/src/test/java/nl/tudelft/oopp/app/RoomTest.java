b11939caf2318b7e6b948398abb57fe547eca6c6
rsozonov <r.n.sozonov@student.tudelft.nl>
2021-03-14 23:05:37 +0200

    Add tests to RoomTest

--- server/src/test/java/nl/tudelft/oopp/app/RoomTest.java
+++ server/src/test/java/nl/tudelft/oopp/app/RoomTest.java
@@ -35,4 +35,49 @@ public class RoomTest {
         Room room2 = roomRepository.getOne(room.getId());
         assertEquals(room, room2);
     }
+
+    /**
+     * Check if the room is close
+     */
+    @Test
+    public void closeRoom(){
+        Room room = new Room("My room");
+        roomRepository.save(room);
+        roomRepository.closeRoom(room.getLinkIdModerator());
+        assertEquals(false,room.isOpen());
+    }
+    /**
+     *Check if the the students has permission to room (if they do not have permission so they are kickked)
+     */
+    @Test
+    public void kickTheStudents(){
+        Room room = new Room("My room");
+        roomRepository.save(room);
+        roomRepository.kickAllStudents(room.getLinkIdModerator());
+        assertEquals(false,room.getPermission());
+    }
+    /**
+     *Check if the other room on the repository is still open
+     */
+    @Test
+    public void closeRoomFalse(){
+        Room room = new Room("My room");
+        Room room2 = new Room("New room");
+        roomRepository.save(room);
+        roomRepository.save(room2);
+        roomRepository.closeRoom(room.getLinkIdModerator());
+        assertEquals(true,room2.isOpen());
+    }
+    /**
+     *Check if the other room is still open for students
+     */
+    @Test
+    public void kickTheStudentsFalse(){
+        Room room = new Room("My room");
+        Room room2 = new Room("New room");
+        roomRepository.save(room);
+        roomRepository.save(room2);
+        roomRepository.kickAllStudents(room.getLinkIdModerator());
+        assertEquals(true,room2.getPermission());
+    }
 }
