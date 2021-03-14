49228b8a902183dff1de4dd45f360e48a97343a5
B <b.machadogonzagabarroso-1@student.tudelft.nl>
2021-03-15 00:36:31 +0100

    Methods to edit UpVotes on the server side

--- server/src/main/java/nl/tudelft/oopp/app/controllers/QuestionController.java
+++ server/src/main/java/nl/tudelft/oopp/app/controllers/QuestionController.java
@@ -60,5 +60,35 @@ public class QuestionController {
         questionService.dismissQuestion(questionId);
     }
 
+    /**
+     * calls the questionService to add an upvote to a specific question by a specific
+     * user on the database.
+     * @param questionId the id of the question to be upVoted
+     * @param userId - Id of the user who upVoted
+     */
+    @PostMapping("/changeUpvote/{questionId}/{userId}")
+    @ResponseBody
+    public void addUpvote(@PathVariable String questionId,
+                    @PathVariable String userId) {
+        System.out.println("Upvote arrived on server!");
+
+        questionService.addUpvote(questionId, userId);
+
+    }
+
+    /**
+     * calls the questionService to delete the upvote from the database.
+     * @param questionId the id of the question to be deleted
+     * @param userId - user who made the change
+     */
+    @DeleteMapping("/deleteUpvote/{questionId}/{userId}")
+    @ResponseBody
+    public void deleteUpvote(@PathVariable("questionId") String questionId,
+                                @PathVariable("userId") String userId) {
+        questionService.deleteUpvote(questionId, userId);
+    }
+
+
+
 
 }
