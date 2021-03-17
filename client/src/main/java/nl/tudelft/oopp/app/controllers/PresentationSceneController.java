13c766ccb4506918c2c24f360d331b9d93955c3e
Mauro Storti <m.storti@student.tudelft.nl>
2021-03-17 22:27:42 +0000

    doc(Client): Add javadoc to initialize method

--- client/src/main/java/nl/tudelft/oopp/app/controllers/PresentationSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/app/controllers/PresentationSceneController.java
@@ -82,6 +82,12 @@ public class PresentationSceneController extends HomeSceneController implements 
         stage.centerOnScreen();
         stage.show();
     }
++/**
+* Method to make it so that when the scene loads
+* the refresh method is automatically executed.
+* @param url - url of the scene
+* @param rb - resource bundle used
+*/
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
