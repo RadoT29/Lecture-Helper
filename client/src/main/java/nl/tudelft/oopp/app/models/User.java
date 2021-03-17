2d0f491a69c7bbc46d597783cfd4880bb835df31
B <b.machadogonzagabarroso-1@student.tudelft.nl>
2021-03-17 12:41:24 +0100

    Fix in User, Moderator and Student classes set up.

--- client/src/main/java/nl/tudelft/oopp/app/models/User.java
+++ client/src/main/java/nl/tudelft/oopp/app/models/User.java
@@ -3,9 +3,9 @@ package nl.tudelft.oopp.app.models;
 import java.util.Date;
 
 public class User {
-    public long id;
+    public String id;
     public String name;
-    public Room roomId;
+    public String roomLink;
     public boolean isModerator;
     //  private Date createdAt;
     //  private Date updatedAt;
@@ -14,35 +14,20 @@ public class User {
      * Constructor for User class.
      * @param id - Id for each user
      * @param name - userName used by a user
-     * @param roomId - roomId of the room where user is
+     * @param roomLink - roomId of the room where user is
      * @param isModerator - boolean to indicate if user is a moderator
      */
-    public User(long id,String name, Room roomId, boolean isModerator) {
+    public User(String id,String name, String roomLink, boolean isModerator) {
         this.id = id;
 
         this.name = name;
 
-        this.roomId = roomId;
+        this.roomLink = roomLink;
 
         this.isModerator = isModerator;
 
     }
 
-    public Long getUserID() {
-        return id;
-    }
-
-    public Room getRoomId() {
-        return roomId;
-    }
-
-    public String getUserName() {
-        return name;
-    }
-
-    public boolean getIsModerator() {
-        return isModerator;
-    }
 
 
 
