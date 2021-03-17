a546cd231313207d9a90ba965b6e38e26ad1d67b
rsozonov <r.n.sozonov@student.tudelft.nl>
2021-03-17 19:23:53 +0100

    Adding classes PresentationSceneController

---
+++ client/src/main/java/nl/tudelft/oopp/app/controllers/PresentationSceneController.java
@@ -0,0 +1,85 @@
+package nl.tudelft.oopp.app.controllers;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import nl.tudelft.oopp.app.communication.HomeSceneCommunication;
+import nl.tudelft.oopp.app.models.Question;
+import nl.tudelft.oopp.app.models.Session;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.PriorityQueue;
+import java.util.ResourceBundle;
+
+/**
+ * This class controls the Main scene of the Moderators.
+ */
+public class PresentationSceneController extends HomeSceneController {
+    @FXML
+    private VBox questionBox;
+    @FXML
+    public VBox mainMenu;
+
+    @Override
+    public void loadQuestions() {
+
+        String resource = "/questionCellPresentation.fxml";
+
+        questionBox.getChildren().clear();
+
+        for (int i = 0; i < 5; i++) {
+            Question question = questions.poll();
+            if (question == null) {
+                break;
+            }
+            try {
+                questionBox.getChildren()
+                        .add(createQuestionCell(question, resource));
+            } catch (IOException e) {
+                questionBox.getChildren().add(
+                        new Label("Something went wrong while loading this question"));
+            }
+        }
+    }
+
+    /**
+     * fill in the priority queue and and load them on the screen.
+     */
+    public void refresh() {
+        super.refresh();
+    }
+
+    public void presenterMode() throws IOException {
+
+    }
+
+    /**
+     * Method to load the main moderator scene.
+     * Makes the scene bigger so the moderator can interacat with all its features
+     * @throws IOException if it cant load the fxml file
+     */
+    public void goToHome() throws IOException {
+        Parent loader = new FXMLLoader(getClass().getResource("/moderatorScene.fxml")).load();
+        Stage stage = (Stage) mainMenu.getScene().getWindow();
+
+        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth() * 0.8;
+        double height = screenSize.getHeight() * 0.8;
+
+        Scene scene = new Scene(loader, width, height);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
