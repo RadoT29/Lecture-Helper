0d52e23eeb8ff9bb1c9b48c0fa695e563d9a2c59
B <b.machadogonzagabarroso-1@student.tudelft.nl>
2021-03-31 23:08:22 +0200

    feat(Create the duration and age) Methods to add the age and duration to a question

--- server/src/main/java/nl/tudelft/oopp/app/models/Question.java
+++ server/src/main/java/nl/tudelft/oopp/app/models/Question.java
@@ -5,7 +5,9 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 
@@ -38,7 +40,13 @@ public class Question {
     private String questionText;
 
     private boolean answered;
+    
+    //corresponds to the time when it was created after start of the room
+    private String duration;
 
+    //corresponds to time since its creation
+    private String ageSeconds;
+    
     @CreationTimestamp
     private LocalDateTime createdAt;
 
