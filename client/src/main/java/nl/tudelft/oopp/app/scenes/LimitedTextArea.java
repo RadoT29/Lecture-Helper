20a80e169b9d4bfdb46625e753b75eaade61dd9c
Natalia Pietnoczko <n.pietnoczko@student.tudelft.nl>
2021-03-31 13:19:02 +0200

    fix(LimitedTextArea): cursor fix

--- client/src/main/java/nl/tudelft/oopp/app/scenes/LimitedTextArea.java
+++ client/src/main/java/nl/tudelft/oopp/app/scenes/LimitedTextArea.java
@@ -2,30 +2,40 @@ package nl.tudelft.oopp.app.scenes;
 
 import javafx.scene.control.TextArea;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+/****************************************************************************************
+*    Title: TextField maxlength
+*    Author: Siggouroglou, G
+*    Date: 2013
+*    Availability: https://stackoverflow.com/questions/15159988/javafx-2-2-textfield-maxlength/15188135#15188135
+****************************************************************************************/
 @NoArgsConstructor
+@Setter
 public class LimitedTextArea extends TextArea {
-    public int limit = 254;
+    public int maxlength = 254;
 
     @Override
     public void replaceText(int start, int end, String text) {
-        super.replaceText(start, end, text);
-        verify();
+        // Delete or backspace user input.
+        if (text.equals("")) {
+            super.replaceText(start, end, text);
+        } else if (getText().length() < maxlength) {
+            super.replaceText(start, end, text);
+        }
     }
 
     @Override
     public void replaceSelection(String text) {
-        super.replaceSelection(text);
-        verify();
-    }
-
-    /**
-     * makes sure that the text in the textarea is not longer than the limit.
-     */
-    public void verify() {
-        if (getText().length() > limit) {
-            setText(getText().substring(0, limit));
+        // Delete or backspace user input.
+        if (text.equals("")) {
+            super.replaceSelection(text);
+        } else if (getText().length() < maxlength) {
+            // Add characters, but don't exceed maxlength.
+            if (text.length() > maxlength - getText().length()) {
+                text = text.substring(0, maxlength - getText().length());
+            }
+            super.replaceSelection(text);
         }
     }
 }
