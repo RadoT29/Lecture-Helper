ed56626fbbfcc492588e76c10d24ba4c45c628d9
Natalia Pietnoczko <n.pietnoczko@student.tudelft.nl>
2021-04-06 15:10:08 +0200

    Fix(Client): limitedTextField for the questionInput

---
+++ client/src/main/java/nl/tudelft/oopp/app/scenes/LimitedTextField.java
@@ -0,0 +1,39 @@
+package nl.tudelft.oopp.app.scenes;
+
+import javafx.scene.control.TextField;
+
+
+/****************************************************************************************
+ *    Title: TextField maxlength
+ *    Author: Siggouroglou, G
+ *    Date: 2013
+ *    Availability: https://stackoverflow.com/questions/15159988/javafx-2-2-textfield-maxlength/15188135#15188135
+ ****************************************************************************************/
+public class LimitedTextField extends TextField {
+
+    public int maxlength = 254;
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        // Delete or backspace user input.
+        if (text.equals("")) {
+            super.replaceText(start, end, text);
+        } else if (getText().length() < maxlength) {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text) {
+        // Delete or backspace user input.
+        if (text.equals("")) {
+            super.replaceSelection(text);
+        } else if (getText().length() < maxlength) {
+            // Add characters, but don't exceed maxlength.
+            if (text.length() > maxlength - getText().length()) {
+                text = text.substring(0, maxlength - getText().length());
+            }
+            super.replaceSelection(text);
+        }
+    }
+}
