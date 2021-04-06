3f821647c070416800592d0a2fc36f806ceaae9b
Natalia Pietnoczko <n.pietnoczko@student.tudelft.nl>
2021-04-06 16:13:30 +0200

    Fix(Client): fix checkstyle and add logo icon

--- client/src/main/java/nl/tudelft/oopp/app/controllers/SceneController.java
+++ client/src/main/java/nl/tudelft/oopp/app/controllers/SceneController.java
@@ -154,8 +154,10 @@ public abstract class SceneController implements Initializable {
         DialogPane dialogPane = alert.getDialogPane();
         javafx.scene.control.Button button = new Button();
         button.setStyle("-fx-min-width: 30; -fx-min-hight:30; -fx-background-color: #000; "
-                +"-fx-shape: \"M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 "
-                + "10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z\"");
+                + "-fx-shape: \"M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 "
+                + "2 2 6.48 2 12s4.48 10 10 10 10-4.48 "
+                + "10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 "
+                + "8-8 8 3.59 8 8-3.59 8-8 8z\"");
         dialogPane.setStyle("-fx-background-color: #17AEDA");
         alert.setGraphic(button);
     }
